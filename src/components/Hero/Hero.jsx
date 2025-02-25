c66cea884e51cf78d9f5e392e0428a157c2c0870
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2025-02-25 16:50:07 +0200

    working on heros

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,11 +7,25 @@ const Hero = () => {
     <section className="hero-wrapper">
         <div className="paddings innerWidth flexCenter hero-container">
             
-            <div className="hero-left">
-                left section
+
+            {/*left section */ }
+            <div className="flexColStart hero-left">
+              <div className="hero-title">
+                <h1>Discover <br /> Most Suitable <br /> Property</h1>
+              </div>
             </div>
 
-            <div className="flexCenter  hero-right">
+            <div className="flexColStart hero-des">
+              <span>Find a variety of properties that suit you very easily</span>
+              <span>Forget all difficulties in finding a residence for you</span>
+            </div>
+
+            <div className="search-bar">
+              search bar
+            </div>
+{/* right section */ }
+
+            <div className="flexColStart  hero-right">
                 <div className="image-container">
                     <img src="./hero-image.png" alt="" />
                 </div>
